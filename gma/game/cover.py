b1b86ba76804057237bab9052d4ef32a72a6ca5a
Can Bülbül <alicanblbl@gmail.com>
2013-08-26 00:40:37 +0300

    s

--- gma/game/cover.py
+++
@@ -1,9 +0,0 @@
-Hello, 
-
-I am a junior year CS student looking for an internship and I found your job offer. I have a passion for developing applications with Python
-(especially web-based) as I have been developing with Python for several years, mostly in non-professional manner. Also, I am using JoliOS about
-a year now and I am very excited that now I have a chance to develop applications for it as an intern. I find HTML5 and related technologies
-breathtaking in such a way that the accomplishments they had in couple of years are enormous. I am trying to get along with these concepts
-in order to take a place in this process of development in web technologies.
-
-So, I can assure you I am right person for this job and eagerly waiting for your reply.
